9e8646c468007c752d1fb8bfe61909abac1126d4
Vasileios Papadopoulos <vspapadopou@protonmail.ch>
2019-11-26 21:01:42 +0200

    Added themes

---
+++ src/themes/dark-theme.js
@@ -0,0 +1,31 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import purple from "@material-ui/core/colors/purple";
+
+const primaryGreen = green[500];
+const accentGreen = green.A200;
+const darkGreen = green[900];
+const primaryPurple = purple[500];
+const accentPurple = purple.A200;
+const darkPurple = purple[900];
+
+export const overridings = {
+  palette: {
+    primary: {
+      light: accentPurple,
+      main: primaryPurple,
+      dark: darkPurple,
+      contrastText: "#fff"
+    },
+    type: "dark",
+    secondary: {
+      light: accentGreen,
+      main: primaryGreen,
+      dark: darkGreen,
+      contrastText: "#fff"
+    }
+  },
+  themeName: "Dark Theme"
+};
+
+export default createMuiTheme(overridings);
