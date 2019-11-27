dd125484d2d59b778c009029f5440be9a5e921e5
Vasileios Papadopoulos <vspapadopou@protonmail.ch>
2019-11-27 09:22:42 +0200

    Used theme provider in storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,13 @@
 import { addDecorator, configure } from '@storybook/react';
 
 import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import ThemeProvider from './theme-provider';
 
 import LightTheme from '../src/themes/light-theme';
 import DarkTheme from '../src/themes/dark-theme';
 
 const themes = [LightTheme, DarkTheme];
-addDecorator(withThemesProvider(themes));
+addDecorator(withThemesProvider(themes, ThemeProvider));
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
