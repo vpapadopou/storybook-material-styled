fc759c01fa0960493ac0f4ebd656aa1b22c055ba
Vasileios Papadopoulos <vspapadopou@protonmail.ch>
2019-11-27 09:13:50 +0200

    Added storybook-addon-styled-component-theme configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { addDecorator, configure } from '@storybook/react';
 
-import StylesDecorator from './styles-decorator';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 
-addDecorator(StylesDecorator);
+import LightTheme from '../src/themes/light-theme';
+import DarkTheme from '../src/themes/dark-theme';
+
+const themes = [LightTheme, DarkTheme];
+addDecorator(withThemesProvider(themes));
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/stories', true, /\.stories\.js$/), module);
