e765582fe8803f9946bec8ef489455ae6597bd6f
Vasileios Papadopoulos <vspapadopou@protonmail.ch>
2019-11-27 20:15:37 +0200

    Modified styled button to load background  color based on the current theme

--- src/components/styled-button.js
+++ src/components/styled-button.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 import MatButton from '@material-ui/core/Button';
 
 const StyledButton = styled(MatButton)`
-  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  background: linear-gradient(
+    45deg,
+    ${props => props.theme.palette.awesomeColors.primary} 30%,
+    ${props => props.theme.palette.awesomeColors.secondary} 90%
+  );
   border-radius: 3px;
   border: 0;
   color: white;
